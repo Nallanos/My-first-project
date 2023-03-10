98adbcbe01514e5c8b2b9a85a0f3eedfb8dda2e2
Nallanos <123402313+Nallanos@users.noreply.github.com>
2023-03-10 13:48:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,128 +1,88 @@
 const form = document.querySelector('#form')
-const ul = document.querySelector('ul')
-const trash = document.querySelector('.btn-sm')
-const labelCheckbox = document.querySelectorAll('.form-check-label')
-const listCheckbox = document.querySelectorAll('.form-check-input')
-const toutes = document.querySelector('.active')
-const aFaire = document.querySelector('.aFaire')
+let todocount=0
+const ul= document.querySelector('.list-group')
 const fait = document.querySelector('.fait')
-const poubelle = document.querySelector('#jesuischiant')
-let pageEtat = document.querySelector('.btn-outline-primary').dataset.filter
-
-let todocount = 0
-let info = []
-
-function filter(etat) {
-  const todolist = document.querySelectorAll('[data-etat]')
-  todolist.forEach((li) => {
-    if (etat === 'all') {
-      li.setAttribute('style', '')
-    }
-    else {
-      if (li.dataset.etat == etat) {
-        li.setAttribute('style', '')
-      }
-      else {
-        li.setAttribute('style', 'display:none!important')
-      }
-    }
-  })
-}
+const aFaire= document.querySelector(".aFaire")
+const toutes= document.querySelector('.Toutes')
+let pageEtat='all'
+let task = []
+console.log(task)
 
 function removeTrash(trash) {
+  todocount--
   trash.addEventListener('click', function (e) {
     e.preventDefault()
     trash.parentNode.remove()
+    task.splice(todocount,1)
+    console.log(todocount)
+  console.log(task)
+  console.log(todocount)
   })
 }
 
-function radio(checkbox) {
-  checkbox.addEventListener('change', function (e) {
-    if (e.currentTarget.checked) {
-      e.currentTarget.parentElement.dataset.etat = 'done'
-    }
-    else {
-      e.currentTarget.parentElement.dataset.etat = 'todo'
-    }
-    filter(pageEtat)
-  })
-
-}
-function AddATask(a) {
-  todocount++
-  const todoID = 'todo-' + todocount;
+function createATask (taskname){
+  console.log(todocount)
+  let todoID= 'todo-'+ todocount
   const NewLi = document.createElement('li')
   NewLi.classList.add('list-group-item', 'd-flex', 'align-items-center')
   NewLi.dataset.etat = 'todo'
   NewLi.draggable = true;
-  NewLi.id = todoID
+
   const NewInput = document.createElement('input')
   NewInput.classList.add('form-check-input', 'todo')
   NewInput.setAttribute('type', 'checkbox')
-  NewInput.id = todoID
 
   const NewLabel = document.createElement('label')
   NewLabel.classList.add('ms-2', 'form-check-label')
   NewLabel.setAttribute('for', 'todo-' + todocount)
+  NewLabel.innerHTML = taskname
 
+  NewLabel.appendChild(NewInput)
 
-  NewLi.appendChild(NewInput)
-  NewLi.appendChild(NewLabel)
-
-  NewLabel.innerHTML = a
   const Label = document.createElement('label')
   Label.classList.add('ms-auto', 'btn', 'btn-danger', 'btn-sm')
-
-
   const i = document.createElement('i')
   i.classList.add('bi-trash')
+
+  Label.appendChild(i)
+  NewLi.appendChild(NewInput)
+  NewLi.appendChild(NewLabel)
+  NewLi.appendChild(Label)  
 
   NewLi.ondragstart = (e) => {
     e.dataTransfer.setData('text/plain', e.target.id)
   }
   NewLi.ondragover = (e) => {
     e.preventDefault()
-    ///
   }
-  NewLi.ondrop = (e) => {
-    //check if the drop is on the same list
-    // console.log(e.dataTransfer.getData('text/plain'));
-    if (e.target.id === e.dataTransfer.getData('text/plain')) {
-      return
-    }
-    //move the element if we are moving it into a LI
-    if (e.target.tagName === 'LI') {
-      const elementSource = document.getElementById(e.dataTransfer.getData('text/plain'))
-      const elementDestination = e.target;
-
-      const rect = elementDestination.getBoundingClientRect();
-      const position = e.clientY - rect.top;
-      if (position <= rect.height / 2) {
-        elementDestination.parentNode.insertBefore(elementSource, elementDestination);
-      } else {
-        elementDestination.parentNode.insertBefore(elementSource, elementDestination.nextSibling);
-      }
-
-    }
-  }
-  Label.appendChild(i)
-  NewLi.appendChild(Label)
-  ul.appendChild(NewLi)
+    
+  task.push({
+    etat: false,
+    name: taskname,
+    htmlcontent: NewLi.outerHTML,
+    todoID: todoID
+  })
   removeTrash(Label)
   radio(NewInput)
+  ul.appendChild(NewLi)
+  console.log(task)
+  todocount++
   filter(pageEtat)
-}3
+  console.log(todocount)
+}
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const data = new FormData(form)
-  const taskname = data.get('title')
-  AddATask(taskname)
-  document.getElementById('champ').value = ''
-});
+form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  const data = new FormData(form); 
+  document.getElementById('champ').value = ''; 
+  var taskname = data.get('title'); 
+
+createATask(taskname)
+})
 
 toutes.addEventListener('click', function () {
   pageEtat = 'all'
+  console.log(pageEtat)
   filter(pageEtat)
 
   aFaire.classList.remove('active')
@@ -132,6 +92,7 @@ toutes.addEventListener('click', function () {
 
 aFaire.addEventListener('click', function () {
   pageEtat = 'todo'
+  console.log(pageEtat)
   filter(pageEtat)
 
   aFaire.classList.add('active')
@@ -142,12 +103,44 @@ aFaire.addEventListener('click', function () {
 fait.addEventListener('click', function () {
   pageEtat = 'done'
   filter(pageEtat)
-
   aFaire.classList.remove('active')
   fait.classList.add('active')
   toutes.classList.remove('active')
 })
 
-for (let checkbox of listCheckbox) {
-  radio(checkbox)
+function filter(etat) {
+  const ul = document.querySelectorAll('[data-etat]')
+  ul.forEach((li) => {
+    if (etat === 'all') {
+      li.setAttribute('style', '')
+    }
+    else {
+      console.log(etat)
+      if (li.dataset.etat == etat) {
+        li.setAttribute('style', '')
+      }
+      else {
+        li.setAttribute('style', 'display:none!important')
+      }
+    }
+  })
 }
+
+function radio(checkbox) {
+  checkbox.addEventListener('change', function (e) {
+    const parentLi = e.currentTarget.parentElement;
+    const taskIndex = task.findIndex(task => task.todoID === parentLi.id);
+    if (e.currentTarget.checked) {
+      parentLi.dataset.etat = 'done';
+      task[taskIndex].etat = true;
+      console.log(task);
+    } else {
+      parentLi.dataset.etat = 'todo';
+      task[taskIndex].etat = false;
+      console.log(task);
+    }
+    filter(pageEtat);
+  });
+}
+
+
